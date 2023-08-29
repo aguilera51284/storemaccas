1fef79b12655d80028b7929a5103acf5f6a53c20
Arturo Aguilera <arturoaguilera@Arturos-MacBook-Pro.local>
2023-08-29 13:40:45 -0600

    feat: add more settings

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -16,9 +16,26 @@ class CustomDocument extends Document {
             rel="stylesheet"
           />
 
-
           <meta name="msapplication-TileColor" content="#00CCFF" />
           <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest"></link>
         </Head>
         <body className="debug-screens">
           <Main />
